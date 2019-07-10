1ec39c8f4953fb0b5dc6f38ffcef68867a6e5589
Masato Miyamoto <m2delta29@gmail.com>
2019-07-10 17:22:44 +0900

    feat: test header change

--- resources/assets/js/components/Example.vue
+++ resources/assets/js/components/Example.vue
@@ -1,3 +1,27 @@
 <template>
-  <div>Example</div>
+  <header>
+    <div class="container text-center">
+      <h1>Quiz 4択クイズ&クイズ徹底解説</h1>
+      <a href="/"><img id="header-logo" src=""></a>
+    </div>
+    <nav class="navbar navbar-default navbar-static-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-ex-collapse">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+          <a class="navbar-brand" id="home" href="/">ホーム</a>
+        </div>
+        <div class="collapse navbar-collapse" id="navbar-ex-collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <li><a href="/login">ログイン</a></li>
+            <li><a href="/register">ユーザー登録</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </header>
 </template>
