7690ba770a154f0943a2475c0a0fdc687f646c18
Masato Miyamoto <project-europa@excite.co.jp>
2019-08-24 21:50:11 +0900

    feat: browser-sync導入

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,16 @@
 let mix = require('laravel-mix');
 
 mix.js('resources/assets/js/app.js', 'public/js')
+	.autoload({
+		"jquery": ['$', 'window.jQuery']
+	})
 	.sass('resources/assets/sass/app.scss', 'public/css', {
 		implementation: require('node-sass')
 	});
+
+mix.browserSync({
+	files: [
+		"public/**/*.*"
+	],
+	proxy: 'http://localhost:8000'
+});
